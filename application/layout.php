33d6e9da463008a962e24e20a1dd8f27e717950d
Adam Bruzon <adam.bruzon@googlemail.com>
2013-03-25 11:19:51 +0000

    Updated layout
    Initial layout update and removed .center from application.css as
    bootstrap now provides .text-center

--- application/layout.php
+++ application/layout.php
@@ -27,13 +27,21 @@
 		<div class="header">
 			<div class="container">
 
-				<h1>Lazy Bear Boilerplate</h1>
+				<div class="navbar">
+					<div class="navbar-inner">
 
-				<ul class="nav nav-pills pull-right">
-					<li <?php if ($router->module === "home") { print 'class="active"'; } ?>><a href="/home">Home</a></li>
-					<li <?php if ($router->module === "about") { print 'class="active"'; } ?>><a href="/about">About</a></li>
-					<li <?php if ($router->module === "contact") { print 'class="active"'; } ?>><a href="/contact">Contact</a></li>
-				</ul>
+						<a href="http://www.lazybearhosting.co.uk">
+							<h1 class="brand">LazyBear<b>Boilerplate</b></h1>
+						</a>
+
+						<ul class="nav nav-pills pull-right">
+							<li <?php if ($router->module === "home") { print 'class="active"'; } ?>><a href="/home">Home</a></li>
+							<li <?php if ($router->module === "about") { print 'class="active"'; } ?>><a href="/about">About</a></li>
+							<li <?php if ($router->module === "contact") { print 'class="active"'; } ?>><a href="/contact">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				
 			</div>
 		</div>
 
