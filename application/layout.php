ee66b79df8811e7d21072241ddaefc3ea509c16c
Adam Bruzon <adam.bruzon@googlemail.com>
2013-03-25 09:01:36 +0000

    Updated link to bootstrap-reponsve
    Updated link for bootstrap-reponsive
    Wrapped the header and footer with divs for styling

--- application/layout.php
+++ application/layout.php
@@ -13,7 +13,7 @@
 
 		<link href='http://fonts.googleapis.com/css?family=Roboto:100,400,300' rel='stylesheet' type='text/css'>
 		<link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+		<link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
 		<link href="/assets/css/application.css" rel="stylesheet" media="screen">
 
 		<script src="http://code.jquery.com/jquery-latest.js"></script>
@@ -24,22 +24,26 @@
 
 	<body class="<?php print $router->module; ?>">
 
-		<div class="container">
+		<div class="header">
+			<div class="container">
 
-			<h1>Lazy Bear Boilerplate</h1>
+				<h1>Lazy Bear Boilerplate</h1>
 
-			<ul class="nav nav-pills pull-right">
-				<li <?php if ($router->module === "home") { print 'class="active"'; } ?>><a href="/home">Home</a></li>
-				<li <?php if ($router->module === "about") { print 'class="active"'; } ?>><a href="/about">About</a></li>
-				<li <?php if ($router->module === "contact") { print 'class="active"'; } ?>><a href="/contact">Contact</a></li>
-			</ul>
+				<ul class="nav nav-pills pull-right">
+					<li <?php if ($router->module === "home") { print 'class="active"'; } ?>><a href="/home">Home</a></li>
+					<li <?php if ($router->module === "about") { print 'class="active"'; } ?>><a href="/about">About</a></li>
+					<li <?php if ($router->module === "contact") { print 'class="active"'; } ?>><a href="/contact">Contact</a></li>
+				</ul>
+			</div>
 		</div>
 
 		<?php require_once $router->page; ?>
 
-		<div class="container">
-			<div class="pull-left">Lazy Bear Creations 2013</div>
-			<div class="pull-right">Version 2.0</div>
+		<div class="footer">
+			<div class="container">
+				<div class="pull-left">Lazy Bear Creations 2013</div>
+				<div class="pull-right">Version 2.0</div>
+			</div>
 		</div>
 
 	</body>
